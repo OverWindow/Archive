99a85ad45f87b9dfaf8b834dcac0a4cfd2df5f4e
OverWindow <101511418+OverWindow@users.noreply.github.com>
2022-09-04 22:25:06 +0900

    0904

--- storm/storm_index.js
+++ storm/storm_index.js
@@ -1,27 +1,42 @@
-function run() {var image = document.getElementById('image');
-image.onload = function() {
-  var engine = new RainyDay({
-    image: this,
-    gravityAngle: Math.PI / 5,
-    blur: 10,
-    enableCollisions: true,
-    fps: 30,
-    enableSizeChange: true,
-    sound: "./sound/rainsound.mp3",
-  });
-  engine.trail = engine.TRAIL_SMUDGE;
-  engine.rain([ [3, 2, 2] ], 100);
-  engine.rain(
-    [
-      [1, 0, 20], 
-      [3, 3, 1],
-      [1, 2, 19]
-    ],                       
-    100);
-     engine.rain([ [0, 2, 200], [3, 3, 1] ], 100);
-  };
-    image.crossOrigin = 'anonymous';
-    image.src = 'https://c2.staticflickr.com/6/5142/5653893318_d79d0828c8_b.jpg?w=1024&h=682&auto=compress&cs=tinysrgb';
+const $soundButton = document.querySelector('.sound');
+const $container = document.querySelector('.container');
+let engine = null;
+
+$container.src = 'https://c2.staticflickr.com/6/5142/5653893318_d79d0828c8_b.jpg?w=1024&h=682&auto=compress&cs=tinysrgb';
+
+$soundButton.addEventListener('click',()=>{
+  run();
+},true);
+
+function run() {
+  var image = document.getElementById('image');
+  image.onload = function() {
+    engine = new RainyDay({
+      image: this,
+      gravityAngle: Math.PI / 5,
+      blur: 10,
+      enableCollisions: true,
+      fps: 30,
+      enableSizeChange: true,
+      sound: "./sound/rainsound.mp3",
+    });
+    engine.trail = engine.TRAIL_SMUDGE;
+    engine.rain([ [3, 2, 2] ], 100);
+    engine.rain(
+      [
+        [1, 0, 20], 
+        [3, 3, 1],
+        [1, 2, 19]
+      ],                       
+      100);
+      engine.rain([ [0, 2, 200], [3, 3, 1] ], 100);
+    };
+      image.crossOrigin = 'anonymous';
+      image.src = 'https://c2.staticflickr.com/6/5142/5653893318_d79d0828c8_b.jpg?w=1024&h=682&auto=compress&cs=tinysrgb';
+      //image.src = './rainy_nashville.jpg';
+      //image.src = './city_night.jpg';
 }
+
+
 
 /* manual: https://mubaidr.js.org/rainyday.js/ */
