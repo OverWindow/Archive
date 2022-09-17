44432047e388e054e34d82fe562ba9cf7238989a
OverWindow <101511418+OverWindow@users.noreply.github.com>
2022-09-18 00:37:41 +0900

    0918

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const $nav_right = document.querySelector(".nav_right");
 const $nav = document.querySelector(".nav");
 
 document.querySelector('button').addEventListener('click', function() {
-    const icon = this.querySelector('i');
-    if (icon.classList.contains('fa-angle-down')) {
-      icon.classList.remove('fa-angle-down');
-      icon.classList.add('fa-angle-up');
-      $nav_middle.classList.toggle('active');
-      $nav_right.classList.toggle('active');
-    } else {
-      icon.classList.remove('fa-angle-up');
-      icon.classList.add('fa-angle-down');
-      $nav_middle.classList.toggle('active');
-      $nav_right.classList.toggle('active');
-    }
-  });
+  const icon = this.querySelector('i');
+  if (icon.classList.contains('fa-angle-down')) {
+    icon.classList.remove('fa-angle-down');
+    icon.classList.add('fa-angle-up');
+    $nav_middle.classList.toggle('active');
+    $nav_right.classList.toggle('active');
+  } else {
+    icon.classList.remove('fa-angle-up');
+    icon.classList.add('fa-angle-down');
+    $nav_middle.classList.toggle('active');
+    $nav_right.classList.toggle('active');
+  }
+});
 
 /* Logo Switch */
 
