17bd6f3c04e08833140eac0a9df2f133fa8c0c72
101511418+OverWindow@users.noreply.github.com <101511418+OverWindow@users.noreply.github.com>
2022-10-28 22:48:33 +0900

    1028

--- animation/p5/p5.js
+++ animation/p5/p5.js
@@ -1,20 +1,43 @@
+const $startButton = document.querySelector('#startButton');
+
 const density = "Ñ@#W$9876543210?!abc;:+=-,._                                    ";
-//const density = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,";
+//const density = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/()1{}[]?-_+~<>i!lI;:,";
 // const density = '       .:-i|=+%O#@'
 // const density = '        .:░▒▓█';
 
 let video;
 let asciiDiv;
+let startButton;
+let gameStart = 0;
 
+/** setup */
 function setup() {
   noCanvas();
+
+  startButton = select('#startButton');
+  startButton.mousePressed(greet);
+
   video = createCapture(VIDEO);
-  video.size(80, 60);
+  video.size(60, 45);
 	video.hide();
   asciiDiv = createDiv();
 }
 
+/** when button clicked */
+function greet() {
+  if(!gameStart) {
+    $startButton.innerHTML = 'Stop';
+  } else {
+    $startButton.innerHTML = 'Start';
+  }
+  gameStart = !gameStart;
+}
+
+/** text video */
 function draw() {
+  if(!gameStart) { // text video stop
+    return; 
+  }
   video.loadPixels();
   let asciiImage = "";
   for (let j = 0; j < video.height; j++) {
