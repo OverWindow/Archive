6b1fd1268e62dc3eb8c493d1a2182707827ff87b
101511418+OverWindow@users.noreply.github.com <101511418+OverWindow@users.noreply.github.com>
2022-11-12 00:20:38 +0900

    11.12

---
+++ README.md
@@ -0,0 +1,9 @@
+###### www.overwindow.com
+
+This is the source code of the site:
+
+www.overwindow.com
+
+# Things to know
+
+- community file is not in this git release only on my FTP server
--- index.js
+++ index.js
@@ -3,31 +3,31 @@ const $nav_right = document.querySelector(".nav_right");
 const $nav = document.querySelector(".nav");
 
 /* when resize menu button */
-document.querySelector('button').addEventListener('click', function() {
-    const icon = this.querySelector('i');
-    if (icon.classList.contains('fa-angle-down')) {
-      icon.classList.remove('fa-angle-down');
-      icon.classList.add('fa-angle-up');
-      $nav_middle.classList.toggle('active');
-      $nav_right.classList.toggle('active');
-    } else {
-      icon.classList.remove('fa-angle-up');
-      icon.classList.add('fa-angle-down');
-      $nav_middle.classList.toggle('active');
-      $nav_right.classList.toggle('active');
-    }
-  });
+document.querySelector("button").addEventListener("click", function () {
+  const icon = this.querySelector("i");
+  if (icon.classList.contains("fa-angle-down")) {
+    icon.classList.remove("fa-angle-down");
+    icon.classList.add("fa-angle-up");
+    $nav_middle.classList.toggle("active");
+    $nav_right.classList.toggle("active");
+  } else {
+    icon.classList.remove("fa-angle-up");
+    icon.classList.add("fa-angle-down");
+    $nav_middle.classList.toggle("active");
+    $nav_right.classList.toggle("active");
+  }
+});
 
 /* Logo Switch */
-let $slides = document.getElementsByClassName('slide');
-let $dots = document.getElementsByClassName('dot');
+let $slides = document.getElementsByClassName("slide");
+let $dots = document.getElementsByClassName("dot");
 let slideIndex = 2;
 
 ShowSlides(slideIndex);
 
 function CurrentSlide(n) {
-  console.log('clicked');
-  ShowSlides(slideIndex = n);
+  console.log("clicked");
+  ShowSlides((slideIndex = n));
 }
 
 function ShowSlides(n) {
@@ -37,22 +37,21 @@ function ShowSlides(n) {
   for (i = 0; i < $dots.length; i++) {
     $dots[i].className = $dots[i].className.replace(" active", "");
   }
-  $slides[slideIndex-1].style.display = "flex";
+  $slides[slideIndex - 1].style.display = "flex";
 
   for (let i = 0; i < $slides.length; i++) {
-    $dots[i].style.width = '0';
-    $dots[i].style.borderRadius = '50%';
+    $dots[i].style.width = "0";
+    $dots[i].style.borderRadius = "50%";
   }
-  $dots[n-1].style.width = '17px';
-  $dots[n-1].style.borderRadius = '5px';
+  $dots[n - 1].style.width = "17px";
+  $dots[n - 1].style.borderRadius = "5px";
 }
 
 /* when resized below 1000px ShowSlide(1) */
-window.addEventListener('resize',function() {
-  if(window.matchMedia("(max-width: 1000px)").matches){
-    console.log('shrink');
+window.addEventListener("resize", function () {
+  if (window.matchMedia("(max-width: 1000px)").matches) {
+    console.log("shrink");
     slideIndex = 1;
     ShowSlides(slideIndex);
   }
-})
-
+});
