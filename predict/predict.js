722337e1d7b50ade818d23c0a502f0fb4fd9ed29
101511418+OverWindow@users.noreply.github.com <101511418+OverWindow@users.noreply.github.com>
2023-06-30 01:56:49 +0900

    20230630

--- predict/predict.js
+++ predict/predict.js
@@ -1,4 +1,3 @@
-const $button = document.querySelector("#button");
 const $date = document.querySelector("#date");
 const $estimation = document.querySelector("#estimation");
 const $slots = document.querySelectorAll(".slot");
@@ -6,41 +5,113 @@ const $slots = document.querySelectorAll(".slot");
 // slots[1] = predicted_price
 // slots[2] = balance
 
-$button.addEventListener("click", GetData);
-
 /* Airtable */
-function GetData() {
+window.onload = function GetData() {
   var Airtable = require("airtable");
   var base = new Airtable({ apiKey: "keyLWe1cpGwL7HfFa" }).base(
     "applEsdrNCI75pjhR"
   );
-
+  let yPredict = [];
+  let lowest;
   base("Table 1")
     .select({
-      // Selecting the first 3 records in Grid view:
-      maxRecords: 1,
+      // Selecting the first 7 records in Grid view:
+      maxRecords: 8,
       view: "Grid view",
     })
     .eachPage(
       function page(records, fetchNextPage) {
         // This function (`page`) will get called for each page of records.
+        let today = records[0];
+        lowest = Number(records[0].get("Predict"));
+        console.log("Retrieved", today.get("date"), today.get("Open"));
+        $date.innerHTML = today.get("date");
+        $slots[0].innerHTML = `Open: ${today.get("Open")} (W)`;
+        $slots[1].innerHTML = `Estimation: ${today.get("Predict")} (W)`;
+        $slots[2].innerHTML = `Balance: ${Math.ceil(today.get("Balance"))} (W)`;
+        $estimation.innerHTML = `${
+          Math.ceil((Number(today.get("MARR")) - 100) * 100) / 100
+        }%`;
 
-        records.forEach(function (record) {
-          console.log("Retrieved", record.get("date"), record.get("Open"));
-          $date.innerHTML = record.get("date");
-          $slots[0].innerHTML = `Open: ${record.get("Open")} (W)`;
-          $slots[1].innerHTML = `Estimation: ${record.get("Predict")} (W)`;
-          $slots[2].innerHTML = `Balance: ${Math.ceil(
-            record.get("Balance")
-          )} (W)`;
-          $estimation.innerHTML = `${
-            Math.ceil((Number(record.get("MARR")) - 100) * 100) / 100
-          }%`;
-        });
-
+        for (let i = 7; i >= 0; i--) {
+          yPredict.push(records[i].get("Predict"));
+          lowest = Math.min(lowest, Number(records[i].get("Predict")));
+        }
         // To fetch the next page of records, call `fetchNextPage`.
         // If there are more records, `page` will get called again.
         // If there are no more records, `done` will get called.
+        fetch("https://api.upbit.com/v1/candles/days?market=KRW-BTC&count=8", {
+          headers: {
+            Accept: "application/json",
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            let xValues = [];
+            let yActual = [];
+            for (let i = 7; i >= 0; i--) {
+              xValues.push(data[i].candle_date_time_kst.slice(5, 10));
+              if (i != 0) {
+                yActual.push(data[i].trade_price);
+                lowest = Math.min(lowest, Number(data[i].trade_price));
+              }
+            }
+            // console.log(xValues);
+            // console.log(yActual);
+            new Chart("myChart", {
+              type: "line",
+              data: {
+                labels: xValues,
+                datasets: [
+                  {
+                    label: "Actual Close",
+                    data: yActual,
+                    borderColor: "rgb(234, 155, 75)",
+                    fill: false,
+                  },
+                  {
+                    label: "Predicted Close",
+                    data: yPredict,
+                    borderColor: "red",
+                    fill: false,
+                  },
+                ],
+              },
+              options: {
+                legend: {
+                  display: true,
+                  position: "bottom",
+                },
+                scales: {
+                  xAxes: [
+                    {
+                      display: true,
+                      scaleLabel: {
+                        display: false,
+                        labelString: "x축",
+                      },
+                    },
+                  ],
+                  yAxes: [
+                    {
+                      display: true,
+                      ticks: {
+                        suggestedMin: lowest,
+                        stepSize: 1000000,
+                      },
+                      scaleLabel: {
+                        display: false,
+                        labelString: "y축",
+                      },
+                      color: "black",
+                    },
+                  ],
+                },
+              },
+            });
+          })
+          .catch((e) => console.log(e));
         fetchNextPage();
       },
       function done(err) {
@@ -50,4 +121,31 @@ function GetData() {
         }
       }
     );
-}
+};
+
+// new Chart("myChart", {
+//   type: "line",
+//   data: {
+//     labels: xValues,
+//     datasets: [
+//       {
+//         data: [860, 1140, 1060, 1060, 1070, 1110, 1330, 2210, 7830, 2478],
+//         borderColor: "red",
+//         fill: false,
+//       },
+//       {
+//         data: [1600, 1700, 1700, 1900, 2000, 2700, 4000, 5000, 6000, 7000],
+//         borderColor: "green",
+//         fill: false,
+//       },
+//       {
+//         data: [300, 700, 2000, 5000, 6000, 4000, 2000, 1000, 200, 100],
+//         borderColor: "blue",
+//         fill: false,
+//       },
+//     ],
+//   },
+//   options: {
+//     legend: { display: false },
+//   },
+// });
